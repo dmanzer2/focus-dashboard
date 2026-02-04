d8f05d3289a01cd402602f106dd5a7ba75194406
David Manzer <dmanzer2@gmail.com>
2026-02-04 10:27:30 -0700

    zoneless change detection

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,12 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    // Using new zoneless feature here for improved performance, Smaller bundle sizes, and better compatibility with Angular's future
+    provideZonelessChangeDetection(),
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes)
   ]
